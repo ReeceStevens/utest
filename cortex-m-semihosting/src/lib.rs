3b3679ef425db4dc4848e321be788aa0e6cd8d4e
Reece Stevens <reecestevens24@gmail.com>
2018-11-15 00:27:06 -0600

    Update semihosting to work on beta

--- cortex-m-semihosting/src/lib.rs
+++ cortex-m-semihosting/src/lib.rs
@@ -1,52 +1,51 @@
-#![feature(asm)]
 #![no_std]
 
 #[macro_use]
 extern crate cortex_m_semihosting;
 
+use cortex_m_semihosting::debug;
+
 #[no_mangle]
 pub fn __test_start(ntests: usize) {
-    ehprintln!("running {} tests", ntests)
+    hprintln!("running {} tests", ntests);
 }
 
 #[no_mangle]
 pub fn __test_ignored(name: &str) {
-    ehprintln!("test {} ... ignored", name);
+    hprintln!("test {} ... ignored", name);
 }
 
 #[no_mangle]
 pub fn __test_before_run(name: &str) {
-    ehprint!("test {} ... ", name);
+    hprint!("test {} ... ", name);
 }
 
 #[no_mangle]
 pub fn __test_panic_fmt(args: ::core::fmt::Arguments,
                         file: &'static str,
                         line: u32) {
-    ehprint!("\npanicked at '");
-    cortex_m_semihosting::io::ewrite_fmt(args);
-    ehprintln!("', {}:{}", file, line);
+    hprint!("\npanicked at '");
+    cortex_m_semihosting::export::hstdout_fmt(args).unwrap();
+    hprintln!("', {}:{}", file, line);
 }
 
 #[no_mangle]
 pub fn __test_failed(_name: &str) {
-    ehprintln!("FAILED");
+    hprintln!("FAILED");
 }
 
 #[no_mangle]
 pub fn __test_success(_name: &str) {
-    ehprintln!("OK");
+    hprintln!("OK");
 }
 
 #[no_mangle]
 pub fn __test_summary(passed: usize, failed: usize, ignored: usize) {
-    ehprintln!("\ntest result: {}. {} passed; {} failed; {} ignored",
+    hprintln!("\ntest result: {}. {} passed; {} failed; {} ignored",
                if failed == 0 { "OK" } else { "FAILED" },
                passed,
                failed,
                ignored);
 
-    unsafe {
-        asm!("bkpt" :::: "volatile");
-    }
+    debug::exit(debug::EXIT_SUCCESS);
 }
